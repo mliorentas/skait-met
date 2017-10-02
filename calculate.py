a3079023ac1aff8f887317d1fd2eb9bbab8ed5e4
mliorentas <matas0911@gmail.com>
2017-10-02 10:24:46 +0300

    changed some flows

--- calculate.py
+++ calculate.py
@@ -4,58 +4,59 @@ from decimal import *
 def FunctionF(number):
     return Decimal(-3* number).exp()
 def FunctionN(number):
-    return Decimal(-3* number).exp() - number
+    return Decimal(-3* number).exp() - Decimal(number)
 def DerivativeN(number):
     return Decimal(-3)* Decimal(-3* number).exp() - Decimal(1)
 
-limit = 100
+limit = 1000
 
 def NewtonMethod(startingPoint, precision):
-    getcontext().prec = 50
+    getcontext().prec = precision
+    precision = 10**(-1 * precision)
+
     nextNumber = Decimal(startingPoint)
     iteration = 1
-    # for x in range (1, 50):
     while abs(FunctionN(nextNumber)) > Decimal(precision):
         if iteration > limit:
             print ("Too much operations")
             return
         nextNumber = nextNumber - FunctionN(nextNumber)/DerivativeN(nextNumber)
-        print (nextNumber)
+        print ("{0}         | {1} | {2}".format(iteration, nextNumber, FunctionN(nextNumber)))
         iteration += 1
     print ("Found. {0}".format(nextNumber))
 
 def FixedPointIterationMethod(startingPoint, precision):
-    getcontext().prec = 50
+    getcontext().prec = precision
+    precision = 10**(-1 * precision)
+
     previousNumber = Decimal(startingPoint)
     nextNumber = FunctionF(previousNumber)
     iteration = 1
-    # for x in range (1, 50):
     while abs(previousNumber - nextNumber) > Decimal(precision):
         if iteration > limit:
             print ("Too much operations")
             return
         previousNumber = nextNumber
         nextNumber = FunctionF(previousNumber)
-        print (nextNumber)
+        print ("{0}         | {1}".format(iteration, nextNumber))
         iteration += 1
     print ("Found. {0}".format(nextNumber))
 
+def Main():
+    print ("Program to calculate e^(-3x) - x = 0 ")
 
-print ("Program to calculate e^(-3x) - x = 0 ")
-startingPoint = 0
-precision = 0.0000001
+    startingPoint = int(input ("Pease input start point. "))
+    precision = int(input ("Pease input precision. "))
+    while True:
+        method = input("Choose method. 1 - Fixed point iteration, 2 - Newton. ")
+        print("Iteration | Aproximation")
+        if method == "1":
+            FixedPointIterationMethod(startingPoint, precision)
+            break
+        elif method == "2":
+            NewtonMethod(startingPoint, precision)
+            break
+        else:
+            print("Wrong input.")
 
-# startingPoint = input ("Pease input start point. ")
-# precision = input ("Pease input precision. ")
-
-while True:
-    # method = input("Choose method. 1 - fixed point iteration, 2 - Newton. ")
-    method = "2"
-    if method == "1":
-        FixedPointIterationMethod(startingPoint, precision)
-        break
-    elif method == "2":
-        NewtonMethod(startingPoint, precision)
-        break
-    else:
-        print("Wrong input.")
+Main()
